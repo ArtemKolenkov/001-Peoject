5f6babb67dd5dbccaca7749169ca0b0c65646232
artemkolenkov <artem.KURN@YANDEX.RU>
2026-04-09 16:15:01 +0300

    LoginTests is added - #01

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,10 +1,4 @@
-package tests;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
