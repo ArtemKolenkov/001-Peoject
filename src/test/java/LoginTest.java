b28e7d0bdd1d399ec4a0d2d9d1dbd47179d6b2da
artemkolenkov <artem.KURN@YANDEX.RU>
2026-04-09 18:56:24 +0300

    Code is formated

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,6 +3,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
 public class LoginTest extends BaseTest {
     @Test
     public void checkLogin() {
